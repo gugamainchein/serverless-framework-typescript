744131b49ab7b82f93722babe9321bcb9030d951
Gustavo Mainchein <gustavomainchein@outlook.com>
2023-04-11 08:12:45 -0300

    fix: remove unused dependences

--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -1,18 +1,3 @@
-// import type {
-//   APIGatewayProxyEvent,
-//   APIGatewayProxyResult,
-//   Handler,
-// } from "aws-lambda";
-// import type { FromSchema } from "json-schema-to-ts";
-
-// type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, "body"> & {
-//   body: FromSchema<S>;
-// };
-// export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
-//   ValidatedAPIGatewayProxyEvent<S>,
-//   APIGatewayProxyResult
-// >;
-
 export const formatJSONResponse = (
   status: number,
   response: Record<string, unknown>
